88c502c2f16a475f75a0533d50b9cabfec0d3780
Koosters <95943323+Koosters@users.noreply.github.com>
2023-10-06 13:30:00 +0200

    Add rps selection
    Added function to add user choice, and the comparison between user/computer

--- script.js
+++ script.js
@@ -3,4 +3,35 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice())
+const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    }
+    if (playerSelection === "rock"){
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock."
+        }
+        else {
+            return "You win! Rock beats scissors."
+        }
+    }
+    if (playerSelection === "paper"){
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beats paper."
+        }
+        else {
+            return "You win! Paper beats rock."
+        }
+    }
+}
+
+function game() {
+    for (i = 0; i<5; i++) {
+        let computerSelection = getComputerChoice();
+        console.log("Computer chose " + computerSelection);
+       console.log(playRound(playerSelection,computerSelection));
+    }
+}
+console.log(game());
